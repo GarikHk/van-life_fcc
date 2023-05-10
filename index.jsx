4b97079e1c7170a2062ae0fc1b423cd12bf0c2ac
Garik H <garik.hakobyan.2004@gmail.com>
2023-05-10 16:53:03 +0400

    fix: fixed about and home page

--- index.jsx
+++ index.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
+// Components
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from './pages/Vans';
+import VanDetail from './pages/VanDetail';
+
+// Server
+import './server'
+
 
 function App() {
   return (
@@ -12,11 +19,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} ></Route>
       </Routes>
     </BrowserRouter>
   )
