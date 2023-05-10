4bb68478babb3f375e1543dc7e33013942a06a1d
Garik H <garik.hakobyan.2004@gmail.com>
2023-05-10 20:49:03 +0400

    Added Host route and fixed some bugs

--- index.jsx
+++ index.jsx
@@ -2,36 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
-// Components
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from './pages/Vans';
-import VanDetail from './pages/VanDetail';
-
 // Server
 import './server'
 
+// Components
+import Layout from "./components/Layout"
+import Home from "./pages/Home"
+import About from "./pages/About"
+// Vans
+import Vans from './pages/Vans/Vans'
+import VanDetail from './pages/Vans/VanDetail'
+// Host
+import HostLayout from './components/HostLayout'
+import Dashboard from './pages/Host/Dashboard'
+import Income from './pages/Host/Income'
+import Reviews from './pages/Host/Reviews'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} ></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="vans" element={<Vans />} />
+                    <Route path="vans/:id" element={<VanDetail />} />
+
+                    {/* Host */}
+                    <Route path="host" element={<HostLayout />}>
+                        <Route index element={<Dashboard />} />
+                        <Route path="income" element={<Income />} />
+                        <Route path="vans" element={<Vans />} />
+                        <Route path="reviews" element={<Reviews />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+    .createRoot(document.getElementById('root'))
+    .render(<App />);
