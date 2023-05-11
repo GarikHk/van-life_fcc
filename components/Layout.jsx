ebafbf65d6b72302f5142b0d6cb6b9d7b555bf13
Garik H <garik.hakobyan.2004@gmail.com>
2023-05-11 12:21:37 +0400

    Added footer and fixed navbars

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout() {
     return (
         <div>
+        <div className="site-wrapper">
             <Header />
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
         </div>
     )
 }
